e6121371df90aae6df6bc8cf46278ecce50a5445
Rishi Senthil Kumar <135209607+Rish1SK@users.noreply.github.com>
2024-04-21 20:00:45 +0530

    Update telco.py

--- telco.py
+++ telco.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sea
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
